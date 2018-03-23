89df68caffa257f5396d58bdde83898019ad1b35
MichaelWalker <MichaelWalker@KIRBYZ240W10-4.ad.nchecr.unsw.edu.au>
2018-03-23 16:35:55 +1100

    Fixed minor buugs and updated LOGGER usage. Corrected indentation.

--- src/reporter/EncounterReporter.java
+++ src/reporter/EncounterReporter.java
@@ -12,178 +12,170 @@ import java.io.* ;
 import java.util.ArrayList ;
 import java.util.HashMap ;
 
-import com.sun.media.jfxmedia.logging.Logger;
 //import java.util.logging.* ;
-import java.util.ArrayList;
+import java.util.logging.Level;
 
 public class EncounterReporter extends Reporter {
 
-	public EncounterReporter(String simname, ArrayList<String> reports) {
-		super(simname, reports);
-		// TODO Auto-generated constructor stub
-	}
+    public EncounterReporter(String simname, ArrayList<String> reports) {
+        super(simname, reports);
+        // TODO Auto-generated constructor stub
+    }
 
-    ArrayList<String> input ;
+    // Was hiding field in Reporter. May yet delete there
+    //ArrayList<String> input ;
 		
-	// Output report
-	ArrayList<?> output ;
+    // Was hiding field in Reporter. May yet delete there
+    // Output report
+    //ArrayList<?> output ;
 
-		// File paths
-		private String logFilePath ;
-		private String errorFilePath ;
-		private String outputFilePath ;
-		
-		private String globalFolder ;
-		
-		// File objects
-		File logFile ;
-		File errorFile ;
-		File outputFile ;
-		
-		// Logger
-		java.util.logging.Logger logger = java.util.logging.Logger.getLogger("reporter") ;
-		
-		/**
-		 * Cycles through encounterReports, finding the agent pairs and storing HashTable 
-		 * of each agent's partners for each cycle
-		 *  
-		 * @return ArrayList<HashMap>
-		 */
-		public ArrayList<HashMap<Integer,ArrayList<Integer>>> preparePartnersReport()
-		{
-			ArrayList<HashMap<Integer,ArrayList<Integer>>> partnersReport = 
-					new ArrayList<HashMap<Integer,ArrayList<Integer>>>() ;
-			ArrayList<String[]> pairArray ;
-			String report ;
-			for (int reportNb = 0 ; reportNb < input.size() ; reportNb += outputCycle )
-			{
-				report = input.get(reportNb) ;
-				logger.info("prepare: " + report);
-				pairArray = reportAgentIdPairs(report) ;
-				partnersReport.add(agentPartners(pairArray)) ;
-			}
-			output = partnersReport ;
-			return partnersReport ;
-		}
-		
-		public ArrayList<String> prepareTransmissionReport()
-		{
-			ArrayList<String> transmissionReport = new ArrayList<String>() ;
-			
-			String report ;
+            // File paths
+            private String logFilePath ;
+            private String errorFilePath ;
+            private String outputFilePath ;
 
-			for (int reportNb = 0 ; reportNb < input.size() ; reportNb += outputCycle )
-			{
-				report = input.get(reportNb) ;
-				logger.info("prepare: " + report) ;
-				transmissionReport.add(encounterByValue("transmission","true",report)) ;
-			}
-			return transmissionReport ;
-		}
-		
-		/**
-		 * From ArrayList of doublets puts agentIds into HashMap whose keys are the agentIds
-		 * and values are arrays of their partners Ids.
-		 * @param pairArray ArrayList<String[]> of agentId doublets indicating sexual encounters 
-		 * @return partnerMap - HashMap indicating partnerIds of each agent (key: agentId)
-		 */
-		private HashMap<Integer,ArrayList<Integer>> agentPartners(ArrayList<String[]> pairArray)
-		{
-			HashMap<Integer,ArrayList<Integer>> partnerMap = new HashMap<Integer,ArrayList<Integer>>() ;
-			int agentNb0 ;
-			int agentNb1 ;
-			for (String[] pairString : pairArray)
-			{ 
-				agentNb0 = Integer.parseInt(pairString[0]) ;
-				agentNb1 = Integer.parseInt(pairString[1]) ;
-				
-				ArrayList<Integer> partnerArray ;
-				if (partnerMap.containsKey(agentNb0))
-				{
-					partnerArray = partnerMap.get(agentNb0) ;
-				}
-				else
-				{
-					partnerArray = new ArrayList<Integer>() ;
-				}
-				partnerArray.add(agentNb1) ;
-				partnerMap.put(agentNb0, partnerArray) ;
+            private String globalFolder ;
 
-				if (partnerMap.containsKey(agentNb1))
-				{
-					partnerArray = partnerMap.get(agentNb1) ;
-				}
-				else
-				{
-					partnerArray = new ArrayList<Integer>() ;
-				}
-				partnerArray.add(agentNb0) ;
-				partnerMap.put(agentNb1, partnerArray) ;
+            // File objects
+            File logFile ;
+            File errorFile ;
+            File outputFile ;
 
-			}
-			return partnerMap ;
-		}
+        // Logger
+        //java.util.logging.Logger logger = java.util.logging.Logger.getLogger("reporter") ;
+
+    /**
+     * Cycles through encounterReports, finding the agent pairs and storing HashTable 
+     * of each agent's partners for each cycle
+     *  
+     * @return ArrayList<HashMap>
+     */
+    public ArrayList<HashMap<Integer,ArrayList<Integer>>> preparePartnersReport()
+    {
+            ArrayList<HashMap<Integer,ArrayList<Integer>>> partnersReport = 
+                            new ArrayList<HashMap<Integer,ArrayList<Integer>>>() ;
+            ArrayList<String[]> pairArray ;
+            String report ;
+            for (int reportNb = 0 ; reportNb < input.size() ; reportNb += outputCycle )
+            {
+                    report = input.get(reportNb) ;
+                    LOGGER.log(Level.INFO, "prepare: {0}", report);
+                    pairArray = reportAgentIdPairs(report) ;
+                    partnersReport.add(agentPartners(pairArray)) ;
+            }
+            return partnersReport ;
+    }
+
+    /**
+     * 
+     * @return String[] report of sexual contacts where STI transmission occurred
+     */    
+    public ArrayList<String> prepareTransmissionReport()
+    {
+        ArrayList<String> transmissionReport = new ArrayList<String>() ;
+
+        String report ;
+
+        for (int reportNb = 0 ; reportNb < input.size() ; reportNb += outputCycle )
+        {
+            report = input.get(reportNb) ;
+            LOGGER.log(Level.INFO, "prepare: {0}", report) ;
+            transmissionReport.add(encounterByValue("transmission","true",report)) ;
+        }
+        return transmissionReport ;
+    }
 		
-		/**
-		 * Extracts pairs of agentId0, agentId1 corresponding to relationships
-		 * @param report
-		 * @return ArrayList of String doublets
-		 */
-		private ArrayList<String[]> reportAgentIdPairs(String report)
-		{
-			logger.info(report);
-			ArrayList<String[]> agentIdPairs = new ArrayList<String[]>() ;
-			for (int startIndex = 0 ; startIndex != -1;)
-			{
-				String[] agentIdPair = extractAgentIds(report,startIndex) ;
-				agentIdPairs.add(agentIdPair) ;
-				
-				// Find startIndex for next loop
-				startIndex = report.indexOf("agentId0", startIndex + 1) ;
-			}
-			return agentIdPairs ;
-		}
-		
-		private String encounterByValue(String propertyName, String value, String report)
-		{
-			int indexStart = 0 ;
-			int indexContact ;
-			String output = "" ;
-			String encounterString ;
-			String contactString ;
-			while (indexStart >= 0)
-			{
-				// Encounter to study
-				encounterString = extractEncounter(report, indexStart) ;
-				
-				// Initialise output for encounter
-				indexContact = encounterString.indexOf("contact:", indexStart) ;
-				String encounterOpening = encounterString.substring(0, indexContact ) ;
-				String encounterOutput = "" ;
-				
-				// check contacts for desired value of propertyName
-				while (indexContact >= 0)
-				{
-					contactString = boundedStringByValue(propertyName,value,"contact:",encounterString) ; 
-					if (!"contact:".equals(contactString)) 
-					{
-						encounterOutput += contactString ;
-					}
-					// Find next contact
-					indexContact = encounterString.indexOf("contact:", indexContact + 1) ;
-				}
-				// Only include encounter in output if any of its contacts are included 
-				if (encounterOutput.length() > 0)
-					output += encounterOpening + encounterOutput ;
-				
-				// Prepare for next loop
-				indexStart = report.indexOf("agentId0:",indexStart+1) ;
-			}
-			// If no positive cases are returned
-			if ("".equalsIgnoreCase(output)) output = "None" ;
-			
-			return output ;
-		}
+    /**
+     * From ArrayList of doublets puts agentIds into HashMap whose keys are the agentIds
+     * and values are arrays of their partners Ids.
+     * @param pairArray ArrayList<String[]> of agentId doublets indicating sexual encounters 
+     * @return partnerMap - HashMap indicating partnerIds of each agent (key: agentId)
+     */
+    private HashMap<Integer,ArrayList<Integer>> agentPartners(ArrayList<String[]> pairArray)
+    {
+        HashMap<Integer,ArrayList<Integer>> partnerMap = new HashMap<Integer,ArrayList<Integer>>() ;
+        int agentNb0 ;
+        int agentNb1 ;
+        for (String[] pairString : pairArray)
+        { 
+            agentNb0 = Integer.parseInt(pairString[0]) ;
+            agentNb1 = Integer.parseInt(pairString[1]) ;
+
+            updateHashMap(agentNb0,agentNb1,partnerMap) ;
+            updateHashMap(agentNb1,agentNb0,partnerMap) ;
+
+        }
+        return partnerMap ;
+    }
+
+    /**
+     * Extracts pairs of agentId0, agentId1 corresponding to relationships
+     * @param report
+     * @return ArrayList of String doublets
+     */
+    private ArrayList<String[]> reportAgentIdPairs(String report)
+    {
+        LOGGER.info(report);
+        ArrayList<String[]> agentIdPairs = new ArrayList<String[]>() ;
+        for (int startIndex = 0 ; startIndex != -1; startIndex = report.indexOf("agentId0", startIndex + 1))
+        {
+            String[] agentIdPair = extractAgentIds(report,startIndex) ;
+            agentIdPairs.add(agentIdPair) ;
+
+            // Find startIndex for next loop
+            //startIndex = report.indexOf("agentId0", startIndex + 1) ;
+        }
+        return agentIdPairs ;
+    }
+
+    /**
+     * Extracts contact reports where propertyName.value = value
+     * @param propertyName
+     * @param value
+     * @param report
+     * @return 
+     */
+    private String encounterByValue(String propertyName, String value, String report)
+    {
+            int indexStart = 0 ;
+            int indexContact ;
+            String methodOutput = "" ;
+            String encounterString ;
+            String contactString ;
+            while (indexStart >= 0)
+            {
+                    // Encounter to study
+                    encounterString = extractEncounter(report, indexStart) ;
+
+                    // Initialise output for encounter
+                    indexContact = encounterString.indexOf("contact:", indexStart) ;
+                    String encounterOpening = encounterString.substring(0, indexContact ) ;
+                    String encounterOutput = "" ;
+
+                    // check contacts for desired value of propertyName
+                    while (indexContact >= 0)
+                    {
+                            contactString = boundedStringByValue(propertyName,value,"contact:",encounterString) ; 
+                            // if contactString contains actual contact information
+                            if (!"contact:".equals(contactString)) 
+                            {
+                                    encounterOutput += contactString ;
+                            }
+                            // Find next contact
+                            indexContact = encounterString.indexOf("contact:", indexContact + 1) ;
+                    }
+                    // Only include encounter in output if any of its contacts are included 
+                    if (encounterOutput.length() > 0)
+                            methodOutput += encounterOpening + encounterOutput ; // Include agentId
+
+                    // Prepare for next loop
+                    indexStart = report.indexOf("agentId0:",indexStart+1) ;
+            }
+            // If no positive cases are returned
+            if ("".equalsIgnoreCase(methodOutput)) methodOutput = "None" ;
+
+            return methodOutput ;
+    }
 
         
 	    /**
