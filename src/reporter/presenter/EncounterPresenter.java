44cc45094ff7e2ec0787d189927cd16062d1d9e0
MichaelWalker <MichaelWalker@KIRBYZ240W10-4.ad.nchecr.unsw.edu.au>
2018-04-05 11:04:18 +1000

    Class to plot data from EncounterReporter.
    Current plot is of number of onward transmission for each subclass of Site

---
+++ src/reporter/presenter/EncounterPresenter.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reporter.presenter;
+
+import reporter.* ;
+
+import java.util.ArrayList ;
+import java.util.HashMap;
+
+/**
+ * Class to plot data from EncounterReporter
+ * @author MichaelWalker
+ */
+public class EncounterPresenter extends Presenter {
+    
+    private EncounterReporter reporter ;
+    
+    public EncounterPresenter()
+    {
+        super() ;   
+    }
+    
+    public EncounterPresenter(String applicationTitle, String chartTitle)
+    {
+        super(applicationTitle, chartTitle);
+    }
+    
+    public EncounterPresenter(String applicationTitle, String chartTitle, EncounterReporter reporter)
+    {
+        super(applicationTitle,chartTitle,reporter) ;
+        setReporter(reporter) ;
+    }
+    
+    /**
+     * Overrides super.setReporter() because reporter is now PopulationReporter
+     * @param reporter 
+     */
+    public void setReporter(EncounterReporter reporter)
+    {
+        this.reporter = reporter ;
+    }
+
+    /**
+     * Generates HashMap whose keys are Site names (String) and values the
+     * number of times an infection has been transmitted from that site
+     */
+    public void plotTransmittingSites(String[] siteNames)
+    {
+        // HashMap to be plotted
+        HashMap<String,Integer> transmittingSites = new HashMap<String,Integer>() ;
+        for (String name : siteNames)
+            transmittingSites.put(name, 0) ;
+
+        // For counting infected Sites
+        int count ;
+        
+        // To record whether given Site was responsible for transmission
+        ArrayList<String> infectedStatus ;
+        
+        ArrayList<String> transmissionReport = reporter.prepareTransmissionReport() ;
+        
+        for (String report : transmissionReport)
+        {
+            for (String name : siteNames)
+            {
+                infectedStatus = Reporter.extractAllValues(name, report, 0) ;
+                count = 0 ;
+                for (String site : infectedStatus)
+                {
+                    count += Integer.valueOf(site) ;
+                }
+                count += transmittingSites.get(name) ;
+                transmittingSites.put(name, count) ;
+            }
+            
+        }
+        
+        plotHashMap("Site","Ongoing transmissions",transmittingSites) ;
+        
+        return ;
+    }
+    
+}
