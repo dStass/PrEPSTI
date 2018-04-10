4687194e5915ee48426179f4f14e6348fa7586f6
MichaelWalker <MichaelWalker@KIRBYZ240W10-4.ad.nchecr.unsw.edu.au>
2018-04-10 16:11:00 +1000

    CHANGED: breakupProbability and encounterProbability now static fields

--- src/community/Monogomous.java
+++ src/community/Monogomous.java
@@ -12,30 +12,37 @@ import com.sun.media.jfxmedia.logging.Logger;
  *
  */
 public class Monogomous extends Relationship {
-    private double breakupProbability = 0.1 ;
+    static double breakupProbability = 0.1 ;
     
 
-	// Part of how the number of sexual encounters is determined.
-	// TODO: Finalise the form of how this is handled
-	private double encounterProbability = 0.6 ;
-    
+    // Part of how the number of sexual encounters is determined.
+    // TODO: Finalise the form of how this is handled
+    static double encounterProbability = 0.6 ;
 
-	// Logger
-	java.util.logging.Logger logger = java.util.logging.Logger.getLogger("longTerm") ;
 
-	/**
-	 * 
-	 */
-	public Monogomous(Agent agent1, Agent agent2) {
-		super(agent1,agent2) ;
-		logger.info("Relationship type " + getRelationship());
-	}
-	
-	/**
-	 * TODO: Finalise the form of how this is handled  
-	 * @return the int number of sexual contacts for a given encounter
-	 */
-	protected int chooseNbContacts()
+    // Logger
+    java.util.logging.Logger logger = java.util.logging.Logger.getLogger("longTerm") ;
+
+    /**
+     * 
+     */
+    public Monogomous() {
+            super() ;
+    }
+
+    /**
+     * 
+     */
+    public Monogomous(Agent agent1, Agent agent2) {
+            super(agent1,agent2) ;
+            logger.info("Relationship type " + getRelationship());
+    }
+
+    /**
+     * TODO: Finalise the form of how this is handled  
+     * @return the int number of sexual contacts for a given encounter
+     */
+    protected int chooseNbContacts()
     {
     	return rand.nextInt(3) + 1 ;
     }
