025ea27b7145efd8e1574bfa2705e9b9d79d0546
MichaelWalker <MichaelWalker@KIRBYZ240W10-4.ad.nchecr.unsw.edu.au>
2018-04-10 16:44:28 +1000

    ADDED: consent() for future Methods to call by passing Object[] args
    abstract getters for orgy-related fields 
    
    abstract Site[] getSiteNames()
    
    
    CHANGED:
    setSites() no longer abstract but constructs Site[] sites from abstract getSiteNames()

--- src/agent/Agent.java
+++ src/agent/Agent.java
@@ -35,30 +35,30 @@ public abstract class Agent {
     private int age ;
 
     // Age beyond which no agents live
-    static int maxLife = 65 ;
+    static int MAX_LIFE = 65 ;
 
     // Need to generate random numbers
-    static Random rand = new Random() ;
+    static Random RAND = new Random() ;
 
     // agentId of next Agent to be created, current number plus one
-    static int nbAgentsCreated = 0 ;
+    static int NB_AGENTS_CREATED = 0 ;
 
     // Probability of screening in a given cycle when symptomatic is false
     static double SCREEN_PROBABILITY = 0.01 ;
 
+    // The maximum number of relationships an agent may be willing to sustain
+    //static int MAX_RELATIONSHIPS = 15;
+
     // Agent subclasses need their own array of Sites e.g. penis, anus, pharynx
     private Site[] sites ;
-
-    // The maximum number of relationships an agent may be willing to sustain
-    static int maxRelationships = 15;
 
     // number of relationships willing to maintain at once
     private int promiscuity ;
 
     // probability of choosing each Relationship subclass
     // odds of Monogomous, Regular and Casual
-    int monogomousOdds = rand.nextInt(11) ;
-    int regularOdds = rand.nextInt(11 - monogomousOdds) ;
+    int monogomousOdds = RAND.nextInt(11) ;
+    int regularOdds = RAND.nextInt(11 - monogomousOdds) ;
     int casualOdds = 10 - monogomousOdds - regularOdds ;
 
     // probability of cheating on a monogomous spouse
@@ -118,21 +118,25 @@ public abstract class Agent {
     static double getInfectProbability(Agent infectedAgent, Agent clearAgent, int infectionStatus,
     		Site infectedSite, Site clearSite)
     {
+        LOGGER.log(Level.SEVERE, "static getInfectProbability() called from Agent.java");
     	return 0.5 ;
     }
 	
+   
+    
     /**
      *  
      * Agent Class  
      */
     public Agent(int startAge)
     {
-            this.agentId = nbAgentsCreated ;
-            nbAgentsCreated++ ;
+            this.agentId = NB_AGENTS_CREATED ;
+            NB_AGENTS_CREATED++ ;
             initAge(startAge) ;
             setSites() ;
 
             //Check that setSites(Site[]) was properly implemented by agent Subclass 
+            // TODO: Remove this check once Agent.setSites() is properly implemented
             if (!(sites.length >= 1)) System.err.println("ERROR: Agent subClass.setSites(Site[]) must initialise Sites") ;
 
             initPromiscuity() ;
@@ -160,9 +164,9 @@ public abstract class Agent {
     private int initAge(int startAge)
     {
         if (startAge == -1) // Choose random age from 16 to 65
-            age = rand.nextInt(50) + 16 ;
+            age = RAND.nextInt(50) + 16 ;
         else if (startAge == 0) // Choose random age from 16 to 20, reaching sexual maturity
-            age = rand.nextInt(5) + 16 ;
+            age = RAND.nextInt(5) + 16 ;
         else
             age = startAge ;
         return age ;
@@ -174,11 +178,6 @@ public abstract class Agent {
 
     public void setAge(int age) {
             this.age = age;
-    }
-
-    protected int getMaxRelationships()
-    {
-            return maxRelationships ;
     }
 
     public ArrayList<Relationship> getCurrentRelationships()
@@ -196,7 +195,7 @@ public abstract class Agent {
      */
     private int initPromiscuity()
     {
-            promiscuity = rand.nextInt(getMaxRelationships()) + 1 ;
+            promiscuity = RAND.nextInt(getMaxRelationships()) + 1 ;
             return promiscuity ;
     }
 
@@ -204,7 +203,27 @@ public abstract class Agent {
     {
             return promiscuity ;
     }
+    
+    	
+    abstract int getMaxRelationships() ;
+    
+    /**
+     * 
+     * @return (int) the number of orgies in a community per cycle
+     */
+    abstract public int getOrgyNumber() ;
+    /**
+     * 
+     * @return (int) number of Agents invited to join any given orgy
+     */
+    abstract public int getOrgySize() ;
 
+    /**
+     * 
+     * @return (double) the probability of Agents joining an orgy when invited
+     */
+    abstract public double getJoinOrgyProbability() ;
+    
     public int getMonogomousOdds()
     {
             return monogomousOdds ;
@@ -225,7 +244,8 @@ public abstract class Agent {
      */
     private void initInfidelity()
     {
-            infidelity = 0.1 * rand.nextInt(getMaxRelationships()) ;
+        int maximum = getMaxRelationships() ;
+            infidelity = 0.1 * RAND.nextInt(maximum)/maximum ;
             return ;
     }
 
@@ -281,14 +301,21 @@ public abstract class Agent {
     {
         return censusFieldNames ;
     }
+
+    /**
+     * Specifies Site.subclass.getSimpleName()
+     * @return 
+     */
+    abstract String[] getSiteNames() ;
     
     /**
      * Adds relevant body Sites to particular subClass of agent, 
      * then puts in a Site[] to call setSites(Site[])
      *  e.g. this.mySite = new MySite() ;
-     * Each such Site requires its own getter(). 
+     * Each such Site requires its own getter() and setter(). 
+     * TODO: Use getSiteNames() to dynamically generate in final Method
      */
-    abstract void setSites() ;
+    //abstract void setSites() ;
 
     /**
      * Sets Site[] sites, called from setSites()
@@ -299,16 +326,47 @@ public abstract class Agent {
             this.sites = sites ;
             return ;
     }
+    
+    //TODO: Remove override in MSM.java and make final
+    protected void setSites() // Site[] sites)
+    {
+        String[] siteNames = getSiteNames() ;
+        String siteName ;
+        String setterName ;
+        Method setMethod ;
+        Site newSite ;
+        //sites = new Site[siteNames.length] ;
+        
+        try
+        {
+            Class agentClazz = Class.forName(agent) ;
+            for (int siteIndex = 0 ; siteIndex < siteNames.length ; siteIndex++)
+            {
+                siteName = siteNames[siteIndex] ;
+                setterName = "set" + siteName ;
+                setMethod = agentClazz.getMethod(setterName, Class.forName(siteName)) ;
+                newSite = Class.forName(siteName).asSubclass(Site.class).newInstance() ;
+                setMethod.invoke(this, newSite) ;
+                sites[siteIndex] = newSite ;
+            }
+        }
+        catch ( Exception e )
+        {
+            LOGGER.log(Level.SEVERE, e.getLocalizedMessage());
+        }
+        return ;
+    }
+
 
     /**
      * 
      * @return Site[] sites
      */
-    public Site[] getSites()
+    final public Site[] getSites()
     {
             return sites ;
     }
-
+    
     /**
      * Used when choosing Site for sexual encounter
      * @return randomly chosen available Site
@@ -418,7 +476,8 @@ public abstract class Agent {
                             successful = (successful && site.treat()) ;
                     }
             }
-            if (successful) clearSymptomatic();
+            if (successful) 
+                clearSymptomatic();
             return successful ;
     }
 
@@ -444,6 +503,19 @@ public abstract class Agent {
     }
 
     /**
+     * Called by Methods using consentArgs() to generate desired data
+     * TODO: Remove middle step if Relationship.subclass and Agent partner not
+     * adequate for decision.
+     * 
+     * @param consentArgs
+     * @return consent(String,Agent)
+     */
+    public boolean consent(Object[] consentArgs)
+    {
+        return consent((String) consentArgs[0],(Agent) consentArgs[1]) ;
+    }
+    
+    /**
      * Whether to enter a proposed relationship of class relationshipClazz .
      * Currently according to whether in a monogomous relationship and 
      * the number of relationships already entered compared to promiscuity.
@@ -456,10 +528,25 @@ public abstract class Agent {
     {
         if (inMonogomous)
         {
-            if (rand.nextDouble() > infidelity) 
+            if (RAND.nextDouble() > infidelity) 
                 return false ;
         }
         return available ;
+    }
+    
+    /**
+     * Probabilistically decide to accept invitation to join orgy
+     * @param args
+     * @return true to join and false otherwise
+     */
+    public boolean joinOrgy(double joinOrgyProbability, Object[] args)
+    {
+        if (inMonogomous)
+            if (RAND.nextDouble() > infidelity)
+                return false ;
+        if (RAND.nextDouble() < joinOrgyProbability)
+            return available ;
+        return false ;
     }
 
     /**
@@ -572,14 +659,17 @@ public abstract class Agent {
     /**
      * Override and call in subclass if details about possible partners are 
      * needed.
+     * TODO: Currently not used but may be in future if Relationship subclass
+     * and Agent partner are not longer enough data
+     * 
      * @param relationshipClazzName
      * @param agent
      * @return String[] args of relationshipClazzName and other Properties 
      * relevant to deciding consent()
      */
-    public String[] consentArgs(String relationshipClazzName, Agent agent) 
+    public Object[] consentArgs(String relationshipClazzName, Agent agent) 
     {
-        String[] consentArgs = {relationshipClazzName} ;
+        Object[] consentArgs = {relationshipClazzName,agent} ;
         return consentArgs ;
     }
 
@@ -731,8 +821,8 @@ public abstract class Agent {
     public boolean death()
     {
         // double risk = Math.exp(-age*Math.log(2)/halfLife) ;
-        double risk = Math.pow(((maxLife - age)/((double) maxLife)),2) ;
-        if (rand.nextDouble() > risk ) 
+        double risk = Math.pow(((MAX_LIFE - age)/((double) MAX_LIFE)),2) ;
+        if (RAND.nextDouble() > risk ) 
         {
             clearRelationships() ;
             return true ;
