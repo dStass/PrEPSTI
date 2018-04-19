6258cad1f35b0e84cfa891ea36b286da1de96bbd
MichaelWalker <MichaelWalker@KIRBYZ240W10-4.ad.nchecr.unsw.edu.au>
2018-04-19 16:30:09 +1000

    ADDED: STI class of infections. Base class STI is generic infection, specific infections will be subclasses. Members of this class contain all parameters relevant to infection transmission, symptomatic status, initial prevalence, etc To be a property of Site, not yet integrated

---
+++ src/infection/STI.java
@@ -0,0 +1,151 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package infection;
+
+/**
+ *
+ * @author MichaelWalker
+ */
+public class STI {
+    
+    // Designated number for Infection Class. Always 0 for base class please.
+    static int INFECTION_NB = 0 ;
+    // Probability of Penis infection being symptomatic
+    static double PENIS_SYMPTOMATIC ;
+    // Probability of Rectum infection being symptomatic
+    static double RECTUM_SYMPTOMATIC ;
+    // Probability of Pharynx infection being symptomatic
+    static double PHARYNX_SYMPTOMATIC ;
+    
+    // Probability of transmission from Penis to Penis
+    static double PENIS_PENIS = 0.1 ;
+    // Probability of transmission from Penis to Rectum
+    static double PENIS_RECTUM = 0.8 ;
+    // Probability of transmission from Penis to Pharynx
+    static double PENIS_PHARYNX = 0.6 ;
+    // Probability of transmission from Rectum to Penis
+    static double RECTUM_PENIS = 0.5 ;
+    // Probability of transmission from Rectum to Rectum
+    static double RECTUM_RECTUM = 0.1 ;
+    // Probability of transmission from Rectum to Pharynx
+    static double RECTUM_PHARYNX = 0.2 ;
+    // Probability of transmission from Pharynx to Penis
+    static double PHARYNX_PENIS = 0.5 ;
+    // Probability of transmission from Pharynx to Rectum
+    static double PHARYNX_RECTUM = 0.5 ;
+    // Probability of transmission from Pharynx to Pharynx
+    static double PHARYNX_PHARYNX = 0.5 ;
+    
+    // Is this STI currently causing an infection?
+    private boolean infectionStatus = false ;
+    
+    // Is this STI currently causing symptoms? (false if !infectionStatus)
+    private boolean symptomatic = false ;
+    
+    /**
+     * Set INFECTION_NB. Always zero for STI Base class
+     * @param nb
+     * @return Description INFECTION_NB or warning if nb != 0
+     */
+    public static String SET_INFECTION_NB(int nb)
+    {
+        if (nb == 0)
+        {
+            return "STI nb:0 " ;
+        }
+        return "WARNING: Infection base class should have INFECTION_NB 0" ;
+    }
+    
+    
+    public static int GET_INFECTION_NB()
+    {
+        if (INFECTION_NB != 0)
+        {
+            // TODO: Replace with proper LOGGER
+            System.out.println("WARNING: Infection base class should have INFECTION_NB 0");
+        }
+        return 0 ;
+    }
+    
+    
+    // Probability of Penis infection being symptomatic
+    public double getPenisSymptomatic()
+    {
+        return PENIS_SYMPTOMATIC ;
+    }
+    
+    // Probability of Rectum infection being symptomatic
+    public double getRectumSymptomatic()
+    {
+        return RECTUM_SYMPTOMATIC ;
+    }
+    
+    // Probability of Pharynx infection being symptomatic
+    public double getPharynxSymptomatic()
+    {
+        return PHARYNX_SYMPTOMATIC ;
+    }
+    
+    // Probability of transmission from Penis to Penis
+    public double getPenisPenisTransmission()
+    {
+        return PENIS_PENIS ;
+    }
+    
+    // Probability of transmission from Penis to Rectum
+    public double getPenisRectumTransmission()
+    {
+        return PENIS_RECTUM ;
+    }
+    
+    // Probability of transmission from Penis to Pharynx
+    public double getPenisPharynxTransmission()
+    {
+        return PENIS_PHARYNX ;
+    }
+    
+    // Probability of transmission from Rectum to Penis
+    public double getRectumPenisTransmission()
+    {
+        return RECTUM_PENIS ;
+    }
+    
+    // Probability of transmission from Rectum to Rectum
+    public double getRectumRectumTransmission()
+    {
+        return RECTUM_RECTUM ;
+    }
+    
+    // Probability of transmission from Rectum to Pharynx
+    public double getRectumPharynxTransmission()
+    {
+        return RECTUM_PHARYNX ;
+    }
+    
+    // Probability of transmission from Pharynx to Penis
+    public double getPharynxPenisTransmission()
+    {
+        return PHARYNX_PENIS ;
+    }
+    
+    // Probability of transmission from Pharynx to Rectum
+    public double getPharynxRectumTransmission()
+    {
+        return PHARYNX_RECTUM ;
+    }
+    
+    // Probability of transmission from Pharynx to Rectum
+    public double getPharynxPharynxTransmission()
+    {
+        return PHARYNX_PHARYNX ;
+    }
+    
+    public String getName()
+    {
+        return "Infection" ;
+    }
+    
+}
