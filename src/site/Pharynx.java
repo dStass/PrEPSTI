db4c1ccc0894edf55420459127d914e0de96f9e3
MichaelWalker <MichaelWalker@KIRBYZ240W10-4.ad.nchecr.unsw.edu.au>
2018-04-20 16:42:20 +1000

    Penis.java Added getter for static SYMPTOMATIC_PROBABILITY Consistent and safe access method for static variables.

--- src/site/Pharynx.java
+++ src/site/Pharynx.java
@@ -10,26 +10,36 @@ package site;
  */
 public class Pharynx extends Site {
 
-	// Constants that vary according to Site subclass
-	// Probability of initial gonorrhoea infection 
-	static double INITIAL = 0.3 ;
-	
-	// Probability of positive symptomatic status if infected
-	static double SYMPTOMATIC = 0.05 ;
-	
-	// Probability of site transmitting infection, if all other probabilities unity
-	static double TRANSMIT = 0.5 ;
+    // Constants that vary according to Site subclass
+    // Probability of initial gonorrhoea infection 
+    static double INITIAL = 0.3 ;
 
-	// Probability of site becoming infected, if all other probabilities unity
+    // Probability of positive symptomatic status if infected
+    static double SYMPTOMATIC_PROBABILITY = 0.05 ;
+
+    // Probability of site transmitting infection, if all other probabilities unity
+    static double TRANSMIT = 0.5 ;
+
+    // Probability of site becoming infected, if all other probabilities unity
     static double RECEIVE = 0.7 ;
      
-	
-	/**
-	 * 
-	 */
-	public Pharynx() 
-	{
-		super() ;
-	}
 
+    /**
+     * 
+     */
+    public Pharynx() 
+    {
+            super() ;
+    }
+
+    /**
+     * 
+     * @return The probability of an infection at this Site causing symptoms
+     */        
+    protected double getSymptomaticProbability()
+    {
+        return SYMPTOMATIC_PROBABILITY ;
+    }
+
+    
 }
