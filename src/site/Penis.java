b4d826f1ee6053913fac97e419f0d4072269465e
MichaelWalker <MichaelWalker@KIRBYZ240W10-4.ad.nchecr.unsw.edu.au>
2018-04-20 16:41:48 +1000

    Penis.java Added getter for static SYMPTOMATIC_PROBABILITY Consistent and safe access method for static variables.

--- src/site/Penis.java
+++ src/site/Penis.java
@@ -11,27 +11,38 @@ import com.sun.media.jfxmedia.logging.Logger;
  */
 public class Penis extends Site {
 
-	// Constants that vary according to Site subclass
-	// Probability of initial gonorrhoea infection 
-	static double INITIAL = 0.3 ;
-	
-	// Probability of positive symptomatic status if infected
-	static double SYMPTOMATIC = 0.9 ;
-	
-	// Probability of site transmitting infection, if all other probabilities unity
-	static double TRANSMIT = 0.9 ;
+    // Constants that vary according to Site subclass
+    // Probability of initial gonorrhoea infection 
+    static double INITIAL = 0.3 ;
 
-	// Probability of site becoming infected, if all other probabilities unity
+    // Probability of positive symptomatic status if infected
+    static double SYMPTOMATIC_PROBABILITY = 0.9 ;
+
+    // Probability of site transmitting infection, if all other probabilities unity
+    static double TRANSMIT = 0.9 ;
+
+    // Probability of site becoming infected, if all other probabilities unity
     static double RECEIVE = 0.5 ;
-     
-	
-	/**
-	 * 
-	 */
-	public Penis() 
-	{
-		super() ;
-		Logger.logMsg(0, this.getSite());
-	}
+
+
+    /**
+     * 
+     */
+    public Penis() 
+    {
+        super() ;
+        Logger.logMsg(0, this.getSite());
+    }
+    
+    /**
+     * 
+     * @return The probability of an infection at this Site causing symptoms
+     */        
+    protected double getSymptomaticProbability()
+    {
+        return SYMPTOMATIC_PROBABILITY ;
+    }
+
+    
 
 }
