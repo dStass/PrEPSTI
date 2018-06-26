83b28e7fb4c2ae3c6d834d73058c4b0333fd0631
MichaelWalker <MichaelWalker@KIRBYZ240W10-4.ad.nchecr.unsw.edu.au>
2018-06-26 15:19:55 +1000

    Constants added to control infection duration and treatment probabilities.

--- src/site/Pharynx.java
+++ src/site/Pharynx.java
@@ -15,8 +15,25 @@ public class Pharynx extends Site {
     //static double INITIAL = 0.3 ;
 
     // Probability of positive symptomatic status if infected
-    static double SYMPTOMATIC_PROBABILITY = 0.05 ;
+    static double SYMPTOMATIC_PROBABILITY = 0.0 ;
 
+    /**
+     * Duration of gonorrhoea infection in Pharynx, 12 weeks.
+     */
+    static int INFECTION_DURATION = 12 * 7 ;
+    
+    /**
+     * Mean duration of symptomatic gonorrhoea infection in Pharynx, 12 days.
+     */
+    static int SYMPTOMATIC_DURATION = 12 ;
+    
+    /**
+     * Probability of seeking treatment in a given cycle if infected with 
+     * gonorrhoea.
+     */
+    static double TREATMENT_PROBABILITY = 0.4 ; // 0.3 ;
+    
+    
 
     /**
      * 
@@ -28,12 +45,37 @@ public class Pharynx extends Site {
 
     /**
      * 
-     * @return The probability of an infection at this Site causing symptoms
+     * @return The probability of an infection at this Site causing symptoms.
      */        
     protected double getSymptomaticProbability()
     {
         return SYMPTOMATIC_PROBABILITY ;
     }
 
+    protected int getInfectionDuration()
+    {
+        return INFECTION_DURATION ;
+    }
+    
+    /**
+     * 
+     * @return Mean duration of symptomatic infection (before treatment).
+     */
+    protected int getSymptomaticDuration()
+    {
+        return SYMPTOMATIC_DURATION ;
+    }
+    
+    /**
+     * 
+     * @return Probability of treatment for an STI being sought and successful.
+     */
+    @Override
+    protected double getTreatmentProbability()
+    {
+        return TREATMENT_PROBABILITY ;
+    }
+
+    
     
 }
