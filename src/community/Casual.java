39b0ee827ad046ce5677770c37dea1386c2cc92a
MichaelWalker <MichaelWalker@KIRBYZ240W10-4.ad.nchecr.unsw.edu.au>
2018-07-05 09:15:00 +1000

    ADDED: Local override of getEncounterProbability(). CHANGED: Now a public class to allow access to from Agent.java.

--- src/community/Casual.java
+++ src/community/Casual.java
@@ -4,26 +4,26 @@ import agent.* ;
 import java.util.logging.Level;
 
 
-class Casual extends Relationship {
+public class Casual extends Relationship {
     
     /** The number of current Casual Relationships. */
     static int NB_CASUAL = 0 ;
     
     /** Probability of breakup() in a given cycle. */
-    static double BREAKUP_PROBABILITY = 1.0 ;
+    static public double BREAKUP_PROBABILITY = 1.0 ;
     
     /** Probability of sexual encounter in a given cycle. */
     static double ENCOUNTER_PROBABILITY = 1.0 ;
     
 
-    protected Casual() 
+    public Casual() 
     {
         super() ;
         NB_CASUAL++ ;
     }
     
 
-    protected Casual(Agent agent1, Agent agent2) 
+    public Casual(Agent agent1, Agent agent2) 
     {
         super(agent1,agent2) ;
         NB_CASUAL++ ;
@@ -31,6 +31,15 @@ class Casual extends Relationship {
     
 
 
+    /**
+     * The probability of any sexual contact in any cycle.
+     */
+    @Override
+    protected double getEncounterProbability()
+    {
+        return ENCOUNTER_PROBABILITY ;
+    }
+    
     /*********************************************************************
      * Casual relationship ends at end of cycle 
      * @return True  
@@ -53,7 +62,7 @@ class Casual extends Relationship {
      * @return (double) the probability of a relationship ending in a given cycle
      */
     @Override
-    protected double getBreakupProbability()
+    public double getBreakupProbability()
     {
         return BREAKUP_PROBABILITY ;
     }
