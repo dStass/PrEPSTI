2a339ea0817c3eb8c22e7cf7e36e09cde4d6e652
MichaelWalker <MichaelWalker@KIRBYZ240W10-4.ad.nchecr.unsw.edu.au>
2018-07-23 10:32:28 +1000

    ADDED plotSitePrevalence(siteNames) plots prevalence at each of siteNames on same graph. plotSiteSymptomPrevalence(siteNames) plots prevalence of symptomatic infection at each of siteNames on same graph. plotIncidencePerCycle() plots the rate if STI incidents per head of population in a given cycle. multiPlotScreening(Object[] options) does multiple infection-related plots on same graph according to options.

--- src/reporter/presenter/ScreeningPresenter.java
+++ src/reporter/presenter/ScreeningPresenter.java
@@ -18,6 +18,10 @@ public class ScreeningPresenter extends Presenter {
     
     private ScreeningReporter reporter ; 
     
+    public String PREVALENCE = "prevalence" ;
+    public String COPREVALENCE = "coprevalence" ;
+    public String SYMPTOMATIC = "symptomatic" ;
+    
     
     public ScreeningPresenter()
     {
@@ -50,7 +54,7 @@ public class ScreeningPresenter extends Presenter {
     public void plotPrevalence()
     {
         ArrayList<Object> prevalenceReport = reporter.preparePrevalenceReport() ;
-        
+        //LOGGER.log(Level.INFO, "{0}", prevalenceReport);
         plotCycleValue("prevalence", prevalenceReport) ;
     }
     
@@ -85,6 +89,20 @@ public class ScreeningPresenter extends Presenter {
     }
     
     /**
+     * Co-plots the population prevalence of infected siteNames over time (cycles).
+     * @param siteNames - Array of Sites to plot for.
+     */
+    public void plotSitePrevalence(String[] siteNames)
+    {
+        ArrayList<ArrayList<Object>> prevalenceReports = new ArrayList<ArrayList<Object>>() ;
+        
+        for (String siteName : siteNames)
+            prevalenceReports.add(reporter.prepareSitePrevalenceReport(siteName)) ;
+        
+        multiPlotCycleValue("prevalence", prevalenceReports, siteNames) ;
+    }
+    
+    /**
      * Plots the population prevalence of symptomatic siteName over time (cycles).
      * @param siteName - Name of Site to plot for.
      */
@@ -93,6 +111,20 @@ public class ScreeningPresenter extends Presenter {
         ArrayList<Object> symptomaticReport = reporter.prepareSitePrevalenceReport(siteName) ;
         
         plotCycleValue("symptomatic", symptomaticReport) ;
+    }
+    
+    /**
+     * Co-plots the population prevalence of symptomatic siteNames over time (cycles).
+     * @param siteNames - Array of Sites to plot for.
+     */
+    public void plotSiteSymptomPrevalence(String[] siteNames)
+    {
+        ArrayList<ArrayList<Object>> symptomaticReports = new ArrayList<ArrayList<Object>>() ;
+        
+        for (String siteName : siteNames)
+            symptomaticReports.add(reporter.prepareSitePrevalenceReport(siteName)) ;
+        
+        multiPlotCycleValue("symptomatic", symptomaticReports, siteNames) ;
     }
     
     /**
@@ -113,9 +145,97 @@ public class ScreeningPresenter extends Presenter {
     {
         ArrayList<Object> coprevalenceReport = reporter.prepareSiteCoPrevalenceReport(siteNames) ;
         
-        plotCycleValue("prevalence", coprevalenceReport) ;
+        plotCycleValue("coprevalence", coprevalenceReport) ;
+    }
+    
+    /**
+     * Plots the rate if STI incidents per head of population in a given cycle.
+     */
+    public void plotIncidencePerCycle()
+    {
+        ArrayList<Object> incidenceReport = reporter.prepareIncidenceReport() ;
+        
+        plotCycleValue("incidence", incidenceReport) ;
+    }
+    
+    /**
+     * Generates above plots and puts them on the same set of axes with a legend.
+     * @param options ArrayList of plot names accompanied by String[] of parameters
+     */
+    public void multiPlotScreening(Object[] options)
+    {
+        ArrayList<ArrayList<Object>> multiPlotReports = new ArrayList<ArrayList<Object>>() ;
+        ArrayList<String> multiPlotNames = new ArrayList<String>() ;
+        ArrayList<String> legend = new ArrayList<String>() ;
+        String legendEntry ;
+        Object option ;
+        Object nextOption ;
+        String[] siteNames ;
+        
+        for (int argIndex = 0 ; argIndex < options.length ; argIndex++ )
+        {
+            option = options[argIndex] ;
+            if (COPREVALENCE.equals(option))
+            {
+                nextOption = options[argIndex + 1] ;
+                while (nextOption.getClass().getSimpleName().equals("String[]"))
+                {
+                    siteNames = (String[]) nextOption ;
+                    multiPlotReports.add(reporter.prepareSiteCoPrevalenceReport(siteNames)) ;
+                    multiPlotNames.add(COPREVALENCE) ;
+                    argIndex++ ;
+                    
+                    legendEntry = "" ;
+                    for (String siteName : siteNames)
+                        legendEntry += siteName + "_"  ;
+                    legend.add(legendEntry + "coprevalence") ; //.substring(0, legendEntry.length() - 1)) ;  // Leave out last "_"
+                    if (argIndex == options.length-1)
+                        break ;
+                    nextOption = options[argIndex + 1] ;
+                }
+            }
+
+            if (PREVALENCE.equals(option) || SYMPTOMATIC.equals(option))
+            {
+                // Total prevalence since not followed by String[] of siteNames
+                if (argIndex == options.length-1 || options[argIndex + 1].getClass().getSimpleName().equals("String"))
+                {
+                    multiPlotReports.add(reporter.preparePrevalenceReport()) ;
+                    multiPlotNames.add((String) option) ;
+                    legend.add("total" + "_" + ((String) option)) ;  
+                }
+                else // if siteNames specified by following argument(s)
+                {
+                    nextOption = (String[]) options[argIndex + 1] ;
+                    while (nextOption.getClass().getSimpleName().equals("String[]"))
+                    {
+                        siteNames = (String[]) nextOption ;
+                        for (String siteName : siteNames)
+                        {
+                            multiPlotReports.add(reporter.prepareSitePrevalenceReport(siteName)) ;
+                            multiPlotNames.add((String) option) ;
+                            legend.add(((String) option) + "_" + siteName) ;  
+                        }
+                        argIndex++ ;
+                        if (argIndex == options.length - 1)
+                            break ;
+                        nextOption = options[argIndex + 1] ;
+                    }
+                }
+            }
+        }
+        String[] legendArray =  new String[legend.size()] ;
+        for (int i = 0 ; i < legendArray.length ; i++ )
+            legendArray[i] = legend.get(i) ;
+        multiPlotCycleValue(multiPlotNames,multiPlotReports,legendArray) ;
+        
     }
     
     
+//            (boolean plotPrevalence, boolean plotSymptomatic, boolean plotCoPrevalence, boolean plotCoSymptomatic, 
+//            boolean plotRectum, boolean plotUrethra, boolean plotPharynx)
+//            boolean plotPharynxSymptomPrevalence, boolean plotIncidence, boolean plotRectumIncidence, boolean plotUrethraIncidents, 
+//            boolean plotPharynxIncidents)
+
     
 }
