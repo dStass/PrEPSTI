a543cb4551969c3b7692f9a7005c9c52f0210e16
MichaelWalker <MichaelWalker@KIRBYZ240W10-4.ad.nchecr.unsw.edu.au>
2019-01-17 16:19:04 +1100

    CHANGED: Which Site is chosen given Site choice of partner. Reduced probability of docking to one in ten.

--- src/agent/MSM.java
+++ src/agent/MSM.java
@@ -127,21 +127,21 @@ abstract public class MSM extends Agent {
     private boolean prepStatus ;
 	
     /** Transmission probabilities per sexual contact from Urethra to Rectum */
-    static double URETHRA_TO_RECTUM = 0.03 ; // 0.01 ;
+    static double URETHRA_TO_RECTUM = 0.047 ; 
     /** Transmission probabilities sexual contact from Urethra to Pharynx. */
-    static double URETHRA_TO_PHARYNX = 0.03 ; 
+    static double URETHRA_TO_PHARYNX = 0.047 ; 
     /** Transmission probabilities sexual contact from Rectum to Urethra. */ 
-    static double RECTUM_TO_URETHRA = 0.03 ; 
+    static double RECTUM_TO_URETHRA = 0.047 ; 
     /** Transmission probabilities sexual contact from Rectum to Pharynx. */
-    static double RECTUM_TO_PHARYNX = 0.03 ; 
+    static double RECTUM_TO_PHARYNX = 0.046 ; 
     /** Transmission probabilities sexual contact in Pharynx to Urethra intercourse. */
-    static double PHARYNX_TO_URETHRA = 0.03 ;
+    static double PHARYNX_TO_URETHRA = 0.047 ;
     /** Transmission probabilities sexual contact in Pharynx to Rectum intercourse. */
-    static double PHARYNX_TO_RECTUM = 0.03 ; 
+    static double PHARYNX_TO_RECTUM = 0.046 ; 
     /** Transmission probabilities sexual contact in Pharynx to Pharynx intercourse (kissing). */
-    static double PHARYNX_TO_PHARYNX = 0.03 ; 
+    static double PHARYNX_TO_PHARYNX = 0.046 ; 
     /** Transmission probabilities sexual contact in Urethra to Urethra intercourse (docking). */
-    static double URETHRA_TO_URETHRA = 0.03 ; 
+    static double URETHRA_TO_URETHRA = 0.047 ; 
     /** Transmission probabilities sexual contact in Rectum to Rectum intercourse. */
     static double RECTUM_TO_RECTUM = 0.003 ; // 0.003 ; 
     
@@ -444,10 +444,10 @@ abstract public class MSM extends Agent {
         }
         else    // if (site.getSite().equals(URETHRA))
         {
-            int index = RAND.nextInt(6) ;
-            if (index < 3)
+            int index = RAND.nextInt(10) ;
+            if (index < 5)
                 return pharynx ;
-            if (index < 5) 
+            if (index < 9) 
                 return rectum ;
             return urethra ;
         }
