0dda14e0c866358337ee8a03f126becf62dca73f
MichaelWalker <MichaelWalker@KIRBYZ240W10-4.ad.nchecr.unsw.edu.au>
2019-01-29 12:10:04 +1100

    CHANGED: Initial prevalence set to 0.01, closer to but slightly above equililbrium value. Leads to smooth, quick convergence.

--- src/site/Urethra.java
+++ src/site/Urethra.java
@@ -11,14 +11,11 @@ package site;
 public class Urethra extends Site {
 
     // Constants that vary according to Site subclass
-    // Probability of initial gonorrhoea infection 
-    // static double INITIAL = 0.3 ;
+    /** Probability of initial gonorrhoea infection */
+    static double INITIAL = 0.01 ;
 
     // Probability of positive symptomatic status if infected
     static double SYMPTOMATIC_PROBABILITY = 0.8 ;
-    
-    /** Probability of Site being infected initially */
-    static double INITIAL = 0.005 ;
     
     /**
      * Duration of gonorrhoea infection in Urethra.
