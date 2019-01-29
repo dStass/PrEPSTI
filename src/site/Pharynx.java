4ff827f90acdbc750556151109612b6b95749d80
MichaelWalker <MichaelWalker@KIRBYZ240W10-4.ad.nchecr.unsw.edu.au>
2019-01-29 12:10:30 +1100

    CHANGED: Initial prevalence set to 0.02, closer to but slightly above equililbrium value. Leads to smooth, quick convergence.

--- src/site/Pharynx.java
+++ src/site/Pharynx.java
@@ -11,15 +11,12 @@ package site;
 public class Pharynx extends Site {
 
     // Constants that vary according to Site subclass
-    // Probability of initial gonorrhoea infection 
-    //static double INITIAL = 0.3 ;
+    /** Probability of initial gonorrhoea infection */
+    static double INITIAL = 0.02 ;
 
-    // Probability of positive symptomatic status if infected
+    /** Probability of positive symptomatic status if infected */
     static double SYMPTOMATIC_PROBABILITY = 0.0 ;
     
-    /** Probability of Site being infected initially */
-    static double INITIAL = 0.01 ;
-
     /**
      * Duration of gonorrhoea infection in Pharynx, 12 weeks.
      */
