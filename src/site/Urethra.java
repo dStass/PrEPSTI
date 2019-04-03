ed8343df47a789644d29daecc2261e036cf49d87
MichaelWalker <MichaelWalker@KIRBYZ240W10-4.ad.nchecr.unsw.edu.au>
2019-04-03 16:13:10 +1100

    ADDED: initScreenCycle() and getters and setters for screenTime, screenCycle as part of Site-specific screening.

--- src/site/Urethra.java
+++ src/site/Urethra.java
@@ -3,6 +3,9 @@
  */
 package site;
 
+import org.apache.commons.math3.distribution.GammaDistribution;
+import static site.Site.RAND;
+
 
 /**
  * @author MichaelWalker
@@ -22,7 +25,7 @@ public class Urethra extends Site {
      * Proper value unknown as almost always treated. 
      * Value for Rectum taken.
      */
-    static int INFECTION_DURATION = 183 ;
+    static int INFECTION_DURATION = 92 ; // 183 ;
     
     static int SYMPTOMATIC_DURATION = 3 ;
     
@@ -33,6 +36,13 @@ public class Urethra extends Site {
      */
     static double TREATMENT_PROBABILITY = 0.6 ; // 0.6 ;
     
+    /** Days between asymptomatic STI screens . */
+    private int screenCycle = 92 ;
+    
+    /** Cycles remaining until next STI screen. */
+    private int screenTime ;
+
+    
     /**
      * 
      */
@@ -42,14 +52,29 @@ public class Urethra extends Site {
     }
     
     /**
-     * Determine whether Urethra initially infected.
-     * @return false
+     * Initialises screenCycle from a Gamma distribution to determine how often 
+     * Rectum is screened, and then starts the cycle in a random place so that 
+     * not every MSM screens his Rectum at the same time.
+     * @param statusHIV
+     * @param prepStatus
      */
-//    @Override
-//    public boolean initialiseInfection()
-//    {
-//        return false ;
-//    }
+    @Override
+    public void initScreenCycle(boolean statusHIV, boolean prepStatus)
+    {
+        if (prepStatus)
+            setScreenCycle(((int) new GammaDistribution(31,1).sample()) + 61) ;
+        else
+        {
+            
+            if (statusHIV)
+                setScreenCycle(((int) new GammaDistribution(4,87).sample())) ;  // (((int) new GammaDistribution(6,58).sample())) ;  // 60.4% screen within a year
+            else
+                setScreenCycle(((int) new GammaDistribution(3,128).sample())) ;  // (((int) new GammaDistribution(4,94).sample())) ;  // 54.2% screen within a year
+            
+        }
+        // Randomly set timer for first STI screen 
+        setScreenTime(RAND.nextInt(getScreenCycle())) ;
+    }
     
     /**
      * 
@@ -98,4 +123,51 @@ public class Urethra extends Site {
         return TREATMENT_PROBABILITY ;
     }
 
+    @Override
+    public void setScreenTime(int time) 
+    {
+        screenTime = time ;
+    }
+
+    @Override
+    public int getScreenTime() 
+    {
+        return screenTime ;
+    }
+
+    @Override
+    public void setScreenCycle(int screen) 
+    {
+        screenCycle = screen ;
+    }
+
+    @Override
+    public int getScreenCycle() 
+    {
+        return screenCycle ;
+    }
+
+    /**
+     * Adjusts per year the screening period.
+     * @param year
+     * @throws Exception 
+     */
+    public void reinitScreenCycle(int year) throws Exception
+    {
+        // Go from 2007
+        // Frequencies, given by per 1000 per year, from 2007-2016
+        // Table 17 ARTB 2016
+        double[] testRates = new double[] {333,340,398,382,383,382,391,419,445,499} ;
+        double testBase ;
+        if (year == 0)
+            testBase = testRates[0] ;
+        else
+            testBase = testRates[year - 1] ;
+        
+        double ratio = testBase/testRates[year] ;
+        int newScreenCycle = (int) Math.ceil(ratio * getScreenCycle()) ;
+        setScreenCycle(newScreenCycle) ;
+    }
+    
+    
 }
