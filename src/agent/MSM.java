bec73f80c96b8f8b07983fed5a599ff803ac36b7
MichaelWalker <MichaelWalker@KIRBYZ240W10-4.ad.nchecr.unsw.edu.au>
2019-04-24 16:36:43 +1000

    CHANGED: Latest transmissionProbabilities.

--- src/agent/MSM.java
+++ src/agent/MSM.java
@@ -149,19 +149,19 @@ public class MSM extends Agent {
     /** Transmission probabilities sexual contact from Urethra to Pharynx. */
     static double URETHRA_TO_PHARYNX = 0.010 ; // 0.035 ; 
     /** Transmission probabilities sexual contact from Rectum to Urethra. */ 
-    static double RECTUM_TO_URETHRA = 0.010 ; // 0.008 ; 
+    static double RECTUM_TO_URETHRA = 0.002 ; // 0.008 ; 
     /** Transmission probabilities sexual contact from Rectum to Pharynx. */
-    static double RECTUM_TO_PHARYNX = 0.003 ;
+    static double RECTUM_TO_PHARYNX = 0.005 ;
     /** Transmission probabilities sexual contact in Pharynx to Urethra intercourse. */
-    static double PHARYNX_TO_URETHRA = 0.005 ;
+    static double PHARYNX_TO_URETHRA = 0.001 ;
     /** Transmission probabilities sexual contact in Pharynx to Rectum intercourse. */
-    static double PHARYNX_TO_RECTUM = 0.020 ; // 0.030 ; 
+    static double PHARYNX_TO_RECTUM = 0.015 ; // 0.030 ; 
     /** Transmission probabilities sexual contact in Pharynx to Pharynx intercourse (kissing). */
-    static double PHARYNX_TO_PHARYNX = 0.008 ; // 0.052 ; 
+    static double PHARYNX_TO_PHARYNX = 0.015 ; // 0.052 ; 
     /** Transmission probabilities sexual contact in Urethra to Urethra intercourse (docking). */
     static double URETHRA_TO_URETHRA = 0.002 ; // 0.005 ; 
     /** Transmission probabilities sexual contact in Rectum to Rectum intercourse. */
-    static double RECTUM_TO_RECTUM = 0.0150 ; // 0.003 ; 
+    static double RECTUM_TO_RECTUM = 0.0003 ; // 0.003 ; 
     
     /** The probability of screening in a given cycle with statusHIV true. */
     static double SCREEN_PROBABILITY_HIV_POSITIVE = 0.0029 ;
