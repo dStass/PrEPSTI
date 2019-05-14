f7b1e9c41c08699dda6ecfe73db03527a9e382c5
MichaelWalker <MichaelWalker@KIRBYZ240W10-4.ad.nchecr.unsw.edu.au>
2019-05-14 13:35:03 +1000

    CHANGED: INITIAL prevalence now .015

--- src/site/Pharynx.java
+++ src/site/Pharynx.java
@@ -15,7 +15,7 @@ public class Pharynx extends Site {
 
     // Constants that vary according to Site subclass
     /** Probability of initial gonorrhoea infection */
-    static double INITIAL = 0.02 ;
+    static double INITIAL = 0.015 ;
 
     /** Probability of positive symptomatic status if infected */
     static double SYMPTOMATIC_PROBABILITY = 0.0 ;
