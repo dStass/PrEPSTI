cf67c345ca38059bd6b1eb3c8fd101aff7f01a56
MichaelWalker <MichaelWalker@KIRBYZ240W10-4.ad.nchecr.unsw.edu.au>
2019-09-20 17:30:56 +1000

    More appropriate initial prevalence

--- src/site/Rectum.java
+++ src/site/Rectum.java
@@ -14,15 +14,15 @@ public class Rectum extends Site {
     // Constants that vary according to Site subclass
     
     /** Probability of Site being infected initially */
-    static double INITIAL = 0.075 ; // 0.080 ; // 0.08 ; // 0.005 ;
+    static double INITIAL = 0.034 ; // 0.075 ; // 0.080 ; // 0.08 ; // 0.005 ;
     
     /** Probability of positive symptomatic status if infected */
     static double SYMPTOMATIC_PROBABILITY = 0.15 ;
 
     /**
-     * Duration of gonorrhoea infection in rectum, 6 months.
+     * Duration of gonorrhoea infection in rectum.
      */
-    static int INFECTION_DURATION = 365 ; // 183
+    static int INFECTION_DURATION = 365 ; // 274 ; // 365 ; // 183
     
     /**
      * Probability of seeking treatment in a given cycle if infected with 
