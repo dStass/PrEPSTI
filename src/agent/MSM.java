db1df2c8ec9f9a1105884d4db01889b3d4bbc577
MichaelWalker <MichaelWalker@KIRBYZ240W10-4.ad.nchecr.unsw.edu.au>
2020-01-14 14:46:30 +1100

    Corrected error in REINIT_TRUST_UNDETECTABLE() which had lacked the final lines to implement the calculated change.
    Standardised calculation and application of changeProbability when switching to a positive to a negative value in REINIT _PROBABILITY_DISCLOSURE_HIV() _TRUST_UNDETECTABLE() _PROPORTION_UNDETECTABLE() .

--- src/agent/MSM.java
+++ src/agent/MSM.java
@@ -184,7 +184,8 @@ public class MSM extends Agent {
             {
                 if (msm.undetectableStatus)
                 {
-                    changeProbability = (oldProbability - newProbability)/oldProbability ;
+                    changeProbability = newProbability/oldProbability ; // (oldProbability - newProbability)/oldProbability ;
+                    // equivalent to correct calculation: RAND > (1 - changeProbability)
                     newStatus = RAND.nextDouble() < changeProbability ;
                     msm.setUndetectableStatus(newStatus) ;
                     report += Reporter.ADD_REPORT_PROPERTY(String.valueOf(msm.getAgentId()), newStatus) ;
@@ -245,12 +246,15 @@ public class MSM extends Agent {
                 if (msm.trustUndetectable)
                     continue ;
                 changeProbability = (trustProbability - lastProbability)/(1 - lastProbability) ;
+                msm.setTrustUndetectable(RAND.nextDouble() < changeProbability);
             }
             else
             {
                 if (!msm.trustUndetectable)
                     continue ;
-                changeProbability = (lastProbability - trustProbability)/lastProbability ;
+                changeProbability = trustProbability/lastProbability ; // (lastProbability - trustProbability)/lastProbability ;
+                // equivalent to correct calculation: RAND > (1 - changeProbability)
+                msm.setTrustUndetectable(RAND.nextDouble() < changeProbability) ; 
             }
         }
         return report ;
@@ -314,8 +318,9 @@ public class MSM extends Agent {
             {
                 if (!msm.discloseStatusHIV)
                     continue ;
-                changeProbability = (oldDiscloseProbability - newDiscloseProbability)/oldDiscloseProbability ;
-                msm.setDiscloseStatusHIV(RAND.nextDouble() > changeProbability);
+                changeProbability = newDiscloseProbability/oldDiscloseProbability ; // (oldDiscloseProbability - newDiscloseProbability)/oldDiscloseProbability ;
+                // equivalent to correct calculation: RAND > (1 - changeProbability)
+                msm.setDiscloseStatusHIV(RAND.nextDouble() < changeProbability);
                 //msm.initSeroStatus(changeProbability) ;
             }
             
@@ -867,23 +872,23 @@ public class MSM extends Agent {
     private boolean riskyStatusRegular ;
     
     /** Transmission probabilities per sexual contact from Urethra to Rectum */
-    static double URETHRA_TO_RECTUM = 0.85 ; 
+    static double URETHRA_TO_RECTUM = 0.75 ; // 0.85 ; 
     /** Transmission probabilities sexual contact from Urethra to Pharynx. */
-    static double URETHRA_TO_PHARYNX = 0.50 ; 
+    static double URETHRA_TO_PHARYNX = 0.3 ; // 0.50 ; 
     /** Transmission probabilities sexual contact from Rectum to Urethra. */
-    static double RECTUM_TO_URETHRA = 0.010 ;
+    static double RECTUM_TO_URETHRA = 0.010 ; // 0.010 ;
     /** Transmission probabilities sexual contact from Rectum to Pharynx. */
-    static double RECTUM_TO_PHARYNX = 0.025 ;
+    static double RECTUM_TO_PHARYNX = 0.030 ; // 0.025 ;
     /** Transmission probabilities sexual contact in Pharynx to Urethra intercourse. */
-    static double PHARYNX_TO_URETHRA = 0.010 ; 
+    static double PHARYNX_TO_URETHRA = 0.003 ; // 0.010 ; 
     /** Transmission probabilities sexual contact in Pharynx to Rectum intercourse. */
-    static double PHARYNX_TO_RECTUM = 0.020 ; 
+    static double PHARYNX_TO_RECTUM = 0.030 ; // 0.020 ; 
     /** Transmission probabilities sexual contact in Pharynx to Pharynx intercourse (kissing). */
-    static double PHARYNX_TO_PHARYNX = 0.055 ; 
+    static double PHARYNX_TO_PHARYNX = 0.040 ; // 0.055 ; 
     /** Transmission probabilities sexual contact in Urethra to Urethra intercourse (docking). */
-    static double URETHRA_TO_URETHRA = 0.020 ; 
+    static double URETHRA_TO_URETHRA = 0.002 ; // 0.020 ; 
     /** Transmission probabilities sexual contact in Rectum to Rectum intercourse. */
-    static double RECTUM_TO_RECTUM = 0.020 ;
+    static double RECTUM_TO_RECTUM = 0.003 ; // 0.020 ;
 
     /** The probability of screening in a given cycle with statusHIV true. */
     static double SCREEN_PROBABILITY_HIV_POSITIVE = 0.0029 ;
@@ -908,9 +913,7 @@ public class MSM extends Agent {
     
     /**
      * Allows changing of SITE_TO_SITE transmission probabilities.
-     * @param infectedSiteName (String) Name of transmitting Site.
-     * @param clearSiteName (String) Name of receiving Site.
-     * @param transmission (double) New value for transmissionProbability.
+     * @param index
      */
     public static void RESET_INFECT_PROBABILITIES(int index)
     {
@@ -1157,7 +1160,8 @@ public class MSM extends Agent {
     }
 
     /**
-     * Initialises riskyStatus and probabilityUseCondom according to RISKY_ODDS, SAFE_ODDS
+     * Initialises riskyStatus and probabilityUseCondom, probabilityUseCondomCasual,
+     * probabilityUseCondomRegular according to RISKY_ODDS, SAFE_ODDS
      * Risky or Safe behaviour correlated with HIV status
      * Keep overall risky behaviour the same
      */
@@ -2094,16 +2098,6 @@ public class MSM extends Agent {
     /**
      * Decides probabilistically whether MSM chooses to use a condom in a given encounter.
      * Choice is based on type of Relationship and properties of msm
-     * @param relationshipClazzName
-     * @param msm
-     * @return true if condom is to be used, false otherwise
-     */
-    //@Override
-    //abstract protected boolean chooseCondom(String relationshipClazzName, Agent msm);
-    
-    
-    /**
-     * Decides probabilistically whether MSM chooses to use a condom in a given encounter.
      * Risky MSM are more likely to use strategies other than condoms.
      * @param relationshipClazzName
      * @param agentPartner
@@ -2128,7 +2122,7 @@ public class MSM extends Agent {
             localRiskyStatus = riskyStatusRegular ;
             localProbabilityUseCondom = probabilityUseCondomRegular ;
         }
-        //localProbabilityUseCondom *= 3 ;
+    
         MSM partner = (MSM) agentPartner ;
         if (localRiskyStatus)
         {
