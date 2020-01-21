76a3fbc65371f101e1f29a74f6122ac8e2bfd53d
MichaelWalker <MichaelWalker@KIRBYZ240W10-4.ad.nchecr.unsw.edu.au>
2020-01-21 15:42:14 +1100

    Latest transmissionProbabilities for calibrated simulations with Regular Relationships having encounters 0.50 days.

--- src/agent/MSM.java
+++ src/agent/MSM.java
@@ -872,23 +872,23 @@ public class MSM extends Agent {
     private boolean riskyStatusRegular ;
     
     /** Transmission probabilities per sexual contact from Urethra to Rectum */
-    static double URETHRA_TO_RECTUM = 0.75 ; // 0.85 ; 
+    static double URETHRA_TO_RECTUM = 0.80 ; // 0.85 ; 
     /** Transmission probabilities sexual contact from Urethra to Pharynx. */
-    static double URETHRA_TO_PHARYNX = 0.3 ; // 0.50 ; 
+    static double URETHRA_TO_PHARYNX = 0.45 ; // 0.50 ; 
     /** Transmission probabilities sexual contact from Rectum to Urethra. */
     static double RECTUM_TO_URETHRA = 0.010 ; // 0.010 ;
     /** Transmission probabilities sexual contact from Rectum to Pharynx. */
     static double RECTUM_TO_PHARYNX = 0.030 ; // 0.025 ;
     /** Transmission probabilities sexual contact in Pharynx to Urethra intercourse. */
-    static double PHARYNX_TO_URETHRA = 0.003 ; // 0.010 ; 
+    static double PHARYNX_TO_URETHRA = 0.005 ; // 0.010 ; 
     /** Transmission probabilities sexual contact in Pharynx to Rectum intercourse. */
-    static double PHARYNX_TO_RECTUM = 0.030 ; // 0.020 ; 
+    static double PHARYNX_TO_RECTUM = 0.025 ; // 0.020 ; 
     /** Transmission probabilities sexual contact in Pharynx to Pharynx intercourse (kissing). */
-    static double PHARYNX_TO_PHARYNX = 0.040 ; // 0.055 ; 
+    static double PHARYNX_TO_PHARYNX = 0.055 ; // 0.055 ; 
     /** Transmission probabilities sexual contact in Urethra to Urethra intercourse (docking). */
-    static double URETHRA_TO_URETHRA = 0.002 ; // 0.020 ; 
+    static double URETHRA_TO_URETHRA = 0.001 ; // 0.020 ; 
     /** Transmission probabilities sexual contact in Rectum to Rectum intercourse. */
-    static double RECTUM_TO_RECTUM = 0.003 ; // 0.020 ;
+    static double RECTUM_TO_RECTUM = 0.001 ; // 0.020 ;
 
     /** The probability of screening in a given cycle with statusHIV true. */
     static double SCREEN_PROBABILITY_HIV_POSITIVE = 0.0029 ;
