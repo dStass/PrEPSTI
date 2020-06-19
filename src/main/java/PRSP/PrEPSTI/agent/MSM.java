cb8b4c70e8623abd1e75f6d751f6359865b31408
dStass <dstiasny@outlook.com>
2020-06-19 13:22:08 +1000

    Reboot from cycle implementation and some StringBuilder performance patch (#11)
    * REBOOT_FROM_CYCLE introduced to configloader
    
    * read reboot cycle from config AND add a new method to generate a population census report to a specific cycle
    
    * Implemented a way to write rebooted data to a new -REBOOT file
    
    * documentation added
    
    * FIX: rebootRandomSeed parses REBOOT_PATH directly from loaded path with ConfigLoader
    
    * FIX: invisible tick marks on line charts, now showing ticks conditional to number of data points available on the X-axis
    
    TODO: implement ticks in years if possible
    
    * Further documentation added
    
    * Further documentation added
    
    * Implemented saving of a -GENERATED reboot up to a specific cycle now includes relationships
    
    * Append $cycleCount to fileName for both -REBOOT and -METADATA files when rebooting
    
    Implemented a way to duplicate -METADATA file with modifications that exists in a passed in hashmap
    
    * FIX: metadata reading, same rebooted age,
    
    Fix the way -METADATA and -REBOOT files are detected
    
    Add sorting to dumpfiles by agentID and relationshipID - only when ConfigLoader.DEBUG is set to true
    -> Added a way to set DEBUG on or off in code
    
    Solved age problem by adding cycle/365 to birth age
    
    * FIX: remove dead agents from $cycle-REBOOT file
    
    * screenTime fix
    
    * Attemp at fixing infections in reboot
    
    * correct infected agents at a specific cycle now shows up in the reboot file
    
    TODO: Fix infectionTime
    TODO: implement the correct incubationTime
    
    * saving relationships at a particular cycle implemented
    
    BUGS: age and screenTime discrepancy in some agents
    
    * Age fixed for most agents
    
    TODO: screenTime for some agents are incorrect
    
    * screenTime fixed for cases where agents have not been treated nor screened, documentation added
    
    * Some code added at an attempt to fix agent age discrepancies
    
    * Syntax fix
    
    * Update .gitignore to include json files
    
    * Small changes to ensure code works with local files
    
    * Update config.json
    
    * Reworked ConfigLoader to only read one .json file specified in config.properties
    
    * Fix how we save REBOOT seeds, some fonts updated when building graphs
    
    * rename REBOOT variables in ConfigLoader, debug timestamps added
    
    * timeStamps added, removed record building in initialiseCommunity
    
    * revert previous commit, allow initialRecord to use StringBuilder instead for a performance increase
    
    * dumpRebootData incorporated with SB, saving 15% of time taken in sim
    
    * Some more StringBuilder implemented, timestamps added and slow code snippet isolated in burnin
    
    * sb optimisation implemented for Community.runEncounters, Relationship.encounter()
    
    * Update .gitignore
    
    * sb optimisation to Agent.getCensusReport
    
    * removal of a second traversal for concatenated strings requiring "clear:" at the end
    
    * Removed outputs

--- src/main/java/PRSP/PrEPSTI/agent/MSM.java
+++ src/main/java/PRSP/PrEPSTI/agent/MSM.java
@@ -875,8 +875,11 @@ public class MSM extends Agent {
      * @return (String) report of Relationships generated
      */
     static public String GENERATE_RELATIONSHIPS(ArrayList<Agent> availableAgentList, String[] relationshipClazzNames)
-    {
+    {   
+        float t1 = System.nanoTime();
         String report = "" ;
+        StringBuilder sbReport = new StringBuilder();
+
         Class<?> relationshipClazz ;
     
         ArrayList<MSM> seroSortList ;
@@ -927,7 +930,8 @@ public class MSM extends Agent {
                     {
                         relationshipClazz = Class.forName("PRSP.PrEPSTI.community." + relationshipClazzName) ;
                         Relationship relationship = (Relationship) relationshipClazz.newInstance() ;
-                        report += relationship.addAgents(msm0, msm1);
+                        // report += relationship.addAgents(msm0, msm1);
+                        sbReport.append(relationship.addAgents(msm0, msm1));
                     }
                     catch( Exception e )
                     {
@@ -967,7 +971,8 @@ public class MSM extends Agent {
                         relationshipClazz = Class.forName("PRSP.PrEPSTI.community." + relationshipClazzName) ;
                         Relationship relationship = (Relationship) relationshipClazz.newInstance() ;
                         //incrementNbRelationships() ;
-                        report += relationship.addAgents(msm0, msm1);
+                        // report += relationship.addAgents(msm0, msm1);
+                        sbReport.append(relationship.addAgents(msm0, msm1));
                     }
                     catch( Exception e )
                     {
@@ -983,6 +988,8 @@ public class MSM extends Agent {
 
             
         }
+        // System.out.println(report.length() + " time=" + String.valueOf((System.nanoTime()-t1) / 1000000000f));
+        report = sbReport.toString();
         return report ;
     }
     
